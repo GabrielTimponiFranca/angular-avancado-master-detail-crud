bfc94dfb2c26602f8de4c9d9c7d4e05f79d7d9b1
Timponi <gabrieltimponi@gmail.com>
2019-02-27 13:41:35 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryDBService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDBService {
+    createDb() {
+        const categories = [
+            { id: 1 , nome: 'Moradia' , description: 'Pagamentos de Contas da Casa' },
+            { id: 2 , nome: 'Saúde' , description: 'Plano de Saúde e Remédios' },
+            { id: 3 , nome: 'Lazer' , description: 'Cinema, parques, praia, etc' },
+            { id: 4 , nome: 'Salário' , description: 'Recebimento de Salário' },
+            { id: 5 , nome: 'Freelas' , description: 'Trabalhos como freelancer' },
+        ];
+
+        return { categories };
+    }
+}
